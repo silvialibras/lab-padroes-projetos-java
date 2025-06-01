c71e282b8a8ecb3e3f72af496b5b1cc3af8584b4
Silvia Libras <silvialibras@gmail.com>
2025-06-01 06:33:19 -0300

    Implementação dos padrões GoF: Singleton, Strategy e Facade

---
+++ src/one/digitalinnovation/gof/singleton/SingletonEager.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "apressado".
+ * @ prof Venilton
+ */
+
+public class SingletonEager {
+
+    private static SingletonEager instancia = new SingletonEager();
+
+    private  SingletonEager() {
+        super();
+        }
+
+    public static SingletonEager getInstancia(){
+        return  instancia;
+    }
+}
+
---
+++ src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "Lazy Holder".
+ *
+ * @see <a href="https://stackoverflow.com/a/24018148">Referência</a>
+ *
+ * @author falvojr
+ */
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder {
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return InstanceHolder.instancia;
+    }
+}
